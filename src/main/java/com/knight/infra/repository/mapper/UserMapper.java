460563c96f23e8e6f18753d40a645848ec1432e0
wss <13522360848@163.com>
2022-05-23 20:34:09 +0800

    添加Mapper信息

---
+++ src/main/java/com/knight/infra/repository/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.knight.infra.repository.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.knight.domain.dos.User;
+
+/**
+ * 
+ * @author wss
+ * @date 2022/5/23 8:11 AM
+ */   
+public interface UserMapper extends BaseMapper<User> {
+}
